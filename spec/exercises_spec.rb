9b8988765e7e3773b53179f81cebb8cece743f9e
Melissa Patterson <melissa.patterson.va@gmail.com>
2014-12-04 17:00:36 -0600

    add ex4 and commit

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -19,3 +19,24 @@ describe 'Exercise 1' do
     expect(result).to eq(3)
   end
 end
+
+describe 'Exercise 2' do
+  it 'returns the 2nd element of an array' do
+    result = Exercises.ex2([1,'cool',3])
+    expect(result).to eq('cool')
+  end
+end
+
+describe 'Exercise 3' do
+  it 'adds all of the elements of an array together' do
+    result = Exercises.ex3([5,5,5,1])
+    expect(result).to eq(16)
+  end 
+end
+
+describe 'Exercise 4' do
+  it 'finds max number in an array' do
+    result = Exercises.ex4([1,2,4,5,1])
+    expect(result).to eq(5)
+  end
+end
